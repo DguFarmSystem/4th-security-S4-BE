493e86520be76ed263b610f99ccd636600827131
dohyunk58 <hedge3x@gmail.com>
2025-05-15 02:29:13 +0900

    init: project intialize

---
+++ src/main/java/farmsystem/memekly/MemeklyApplication.java
@@ -0,0 +1,13 @@
+package farmsystem.memekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeklyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/farmsystem/memekly/MemeklyApplicationTests.java
@@ -0,0 +1,13 @@
+package farmsystem.memekly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
