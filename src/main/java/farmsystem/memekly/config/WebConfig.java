6446858ea5c10b1851ef0df843cc738ea6bfb9ff
dohyunk58 <hedge3x@gmail.com>
2025-05-31 15:52:14 +0900

    chore: Add vercel URL for CORS

--- src/main/java/farmsystem/memekly/config/WebConfig.java
+++ src/main/java/farmsystem/memekly/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // api 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000", "https://4th-security-s4-fe-8om6.vercel.app/") // 허용 Origin
+                .allowedOrigins("http://localhost:3000", "https://4th-security-s4-fe-8om6.vercel.app/", "https://4th-security-s4-fe.vercel.app") // 허용 Origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 자격 증명(쿠키, HTTP 인증) 허용 여부
